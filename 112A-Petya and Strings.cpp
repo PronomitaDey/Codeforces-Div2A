cfe86b8de77e18f3ca796ed6dfaa82d52bb0042c
Pronomita Dey <dey.pronomita@gmail.com>
2017-10-03 22:52:59 +0530

    Add files via upload

---
+++ 112A-Petya and Strings.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	char s1[n],s2[n];
+	cin>>s1>>s2;
+	int check=0,diff;
+	for(int i=0;i<n;i++){
+		diff=s1[i]-s2[i];
+		if(diff==0 || diff==32)
+			check=1;
+		else 
+            check=strcmp(s1[i],s2[i]);			
+	}
+	cout<<check<<endl;
+	
+	return 0;
+}
+				
